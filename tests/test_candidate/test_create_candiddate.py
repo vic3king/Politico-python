544214e1689d37383479a45638381741fa0f0eff
AKaniru Victory <vic3coorp@gmail.com>
2019-09-02 13:24:55 +0100

    Fix failing tests on candidates mutation

--- tests/test_candidate/test_create_candiddate.py
+++ tests/test_candidate/test_create_candiddate.py
@@ -7,7 +7,8 @@ from fixtures.candidate.candidate_fixture import (
     create_candidate_mutatation,
     create_candidate_mutatation_response,
     response_when_candidate_exists,
-    response_when_party_already_has_candidate
+    response_when_party_already_has_candidate,
+    create_duplicat_candidate_mutatation
 )
 
 import sys
@@ -19,33 +20,33 @@ class TestCreateCandidate(BaseTestCase):
 
     def test_create_candidate_mutation(self):
         """
-        Testing mutation to create a candidate to run for an opffice
+        Testing mutation to create a candidate to run for an office
         """
 
-        # CommonTestCases.politician_token_assert_equal(
-        #     self,
-        #     create_candidate_mutatation,
-        #     create_candidate_mutatation_response
-        # )
+        CommonTestCases.politician_token_assert_equal(
+            self,
+            create_candidate_mutatation,
+            create_candidate_mutatation_response
+        )
 
-    # def test_create_candidate_when_candidate_is_already_runnign(self):
-    #     """
-    #     Testing mutation to create a candidate a candidate when the candidate is already running for office
-    #     """
+    def test_create_candidate_when_candidate_is_already_running(self):
+        """
+        Testing mutation to create a candidate a candidate when the candidate is already running for office
+        """
 
-    #     CommonTestCases.politician_token_assert_equal(
-    #         self,
-    #         create_candidate_mutatation,
-    #         response_when_candidate_exists
-    #     )
+        CommonTestCases.dup_politician_token_assert_equal(
+            self,
+            create_duplicat_candidate_mutatation,
+            response_when_candidate_exists
+        )
 
-    # def test_create_candidate_when_party_has_candidate(self):
-    #     """
-    #     Testing mutation to create a candidate a candidate when the running party already has a candidate for that office
-    #     """
+    def test_create_candidate_when_party_has_candidate(self):
+        """
+        Testing mutation to create a candidate when the running party already has a candidate for that office
+        """
 
-    #     CommonTestCases.second_politician_token_assert_equal(
-    #         self,
-    #         create_candidate_mutatation,
-    #         create_candidate_mutatation_response
-    #     )
+        CommonTestCases.politician_token_assert_equal(
+            self,
+            create_duplicat_candidate_mutatation,
+            response_when_party_already_has_candidate,
+        )
